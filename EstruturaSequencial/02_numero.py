dab0b2282bbd2be4964aca9aab74fbf953108aaa
Maytê <maytearaujo@yahoo.com.br>
2023-11-12 09:31:38 -0300

    Exercícios Python Estrutura Sequencial

---
+++ EstruturaSequencial/02_numero.py
@@ -0,0 +1,4 @@
+numero = input('Informe um número: ');
+numero = int(numero);
+print("O número informado foi: "+ str(numero));
+# print(f'O número informado foi: {numero}');
---
+++ EstruturaSequencial/03_soma.py
@@ -0,0 +1,6 @@
+n1 = int(input(' informe o 1º número: '));
+n2 = int(input("Informe o 2º número: "));
+
+soma = n1 + n2;
+
+print('A soma de ' + str(n1) + ' e ' + str(n2) + ' é : ' + str(soma))
